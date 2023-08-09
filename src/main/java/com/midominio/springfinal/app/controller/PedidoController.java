5f88cf18a03c1f36b62ac05d1ff842ad195f5306
robe <robe@example.com>
2023-08-09 10:31:11 +0200

    1

--- src/main/java/com/midominio/springfinal/app/controller/PedidoController.java
+++ src/main/java/com/midominio/springfinal/app/controller/PedidoController.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.midominio.springfinal.app.model.Articulo;
+import com.midominio.springfinal.app.model.LineaPedido;
 import com.midominio.springfinal.app.model.Pedido;
 import com.midominio.springfinal.app.model.Tienda;
 import com.midominio.springfinal.app.service.ArticuloService;
@@ -53,4 +57,32 @@ public class PedidoController {
 		public List<Articulo> cargarArticulos(@PathVariable String text){
 		return articuloService.findByTipoText(text);
 	}
+	/*
+	@PostMapping("/form")
+	public String guardar(Pedido pedido,
+			@RequestParam(name = "item_id[]", required = false) Long[] itemId,
+			@RequestParam(name = "cantidad[]", required = false) Integer[] cantidad, 
+			RedirectAttributes flash, 
+			SessionStatus status) {
+			
+		for (int i = 0; i < itemId.length; i++) {
+			Articulo articulo = tiendaService.findArticuloById(itemId[i]);
+
+			LineaPedido linea = new LineaPedido();
+			linea.setCantidad(cantidad[i]);
+			linea.setArticulo(articulo);
+			pedido.addLineaPedido(linea);
+
+			log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+		}
+				
+		tiendaService.guardarPedido(pedido);
+		status.setComplete();
+
+		flash.addFlashAttribute("success", "Pedido creado con éxito!");
+		
+		// Para mostrar detalle de tienda mostrando estado de sus pedidos
+		return "redirect:/tiendas/ver/" + pedido.getTienda().getId();
+	}
+	*/
 }
